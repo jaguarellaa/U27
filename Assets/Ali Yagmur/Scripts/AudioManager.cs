5171cde2ccaf36f17d88f6f1380449e68c7ef2de
AliYagmurGames <aliyagmur.erad@gmail.com>
2023-04-09 10:54:38 +0300

    Audio Is under development
    Audio Is under development

---
+++ Assets/Ali Yagmur/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public List<AudioClip> audioClips;  // List of audio clips to play
+
+    private AudioSource audioSource;   // Reference to the audio source component
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio(int index)
+    {
+        if (index < 0 || index >= audioClips.Count)
+        {
+            Debug.LogError("Invalid audio clip index.");
+            return;
+        }
+        audioSource.Stop();
+        audioSource.clip = audioClips[index];
+        audioSource.Play();
+    }
+
+    public void StopAudio()
+    {
+        audioSource.Stop();
+    }
+
+}
